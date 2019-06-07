8edb9877cdc6f57de0ff053cac15ec9d6d2fedff
gbbenson68 <gbbenson68@gmail.com>
2019-06-07 14:21:19 -0400

    Add modified curl scripts
    Added modified curl scripts.

--- curl-scripts/auth/change-password.sh
+++ curl-scripts/auth/change-password.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
 
-curl "http://localhost:4741/change-password" \
+if [ $# -ne 2 ]
+then
+  echo
+  echo "     Usage: ${0} <URL> <TOKEN>"
+  echo
+  exit 1
+fi
+
+URL=${1}
+TOKEN=${2}
+
+echo -n "Enter old password: "
+stty -echo
+read OLDPASS
+stty echo
+echo
+
+echo -n "Enter new password: "
+stty -echo
+read NEWPASS
+stty echo
+echo
+#echo ${TOKEN}
+#echo ${OLDPASS}
+#echo ${NEWPASS}
+
+curl "${URL}/change-password" \
   --include \
   --request PATCH \
   --header "Authorization: Token token=${TOKEN}" \
   --header "Content-Type: application/json" \
   --data '{
     "passwords": {
-      "old": "'"${OLDPW}"'",
-      "new": "'"${NEWPW}"'"
+      "old": "'"${OLDPASS}"'",
+      "new": "'"${NEWPASS}"'"
     }
   }'
 
--- curl-scripts/auth/sign-out.sh
+++ curl-scripts/auth/sign-out.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-curl "http://localhost:4741/sign-out" \
+if [ $# -ne 2 ]
+then
+  echo
+  echo "     Usage: ${0} <URL> <TOKEN>"
+  echo
+  exit 1
+fi
+
+URL=${1}
+TOKEN=${2}
+
+#echo ${URL}
+#echo ${TOKEN}
+
+curl "${URL}/sign-out" \
   --include \
   --request DELETE \
-  --header "Content-Type: application/json" \
   --header "Authorization: Token token=${TOKEN}"
 
 echo
